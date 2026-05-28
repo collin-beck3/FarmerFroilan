e7a42dff92d218a59de37e8290ab7aa394bbf766
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-05-28 19:02:49 -0400

    added a lot

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -0,0 +1,35 @@
+package com.zipcodewilmington.froilansfarm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Farm {
+    private Field field;
+    private List<Stable> stables;
+
+    public Farm() {
+        field = new Field();
+        stables = new ArrayList<>();
+        // stables.add(new Stable());
+        // stables.add(new Stable());
+        // stables.add(new Stable());
+        // stables.get(0).addHorse(new Horse("Horse1"));
+        // stables.get(0).addHorse(new Horse("Horse2"));
+        // stables.get(0).addHorse(new Horse("Horse3"));
+        // stables.get(1).addHorse(new Horse("Horse4"));
+        // stables.get(1).addHorse(new Horse("Horse5"));
+        // stables.get(1).addHorse(new Horse("Horse6"));
+        // stables.get(2).addHorse(new Horse("Horse7"));
+        // stables.get(2).addHorse(new Horse("Horse8"));
+        // stables.get(2).addHorse(new Horse("Horse9"));
+        // stables.get(2).addHorse(new Horse("Horse10"));
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public List<Stable> getStables() {
+        return stables;
+    }
+}
